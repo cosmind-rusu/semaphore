6555f2c3da263f3878301f0b662221bb037d431e
fiftin <denguk@gmail.com>
2024-03-25 18:58:44 +0100

    feat(billing): download page

--- web/src/views/project/Install.vue
+++ web/src/views/project/Install.vue
@@ -29,7 +29,7 @@
       </v-tab>
     </v-tabs>
 
-    <div class="pa-4">
+    <div class="pa-4" v-if="false">
       <v-alert
         dense
         text
@@ -39,12 +39,121 @@
         You don't have an activated plan.
       </v-alert>
     </div>
+    <div class="pa-4" v-else>
+      <div v-for="version of versions" :key="version.id">
+
+        <h2 class="pl-4">
+          {{ version.semver }}
+          <v-chip small class="ml-2 mb-1" color="error">Premium</v-chip>
+        </h2>
+
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+            <tr>
+              <th class="text-left">
+                Platform
+              </th>
+              <th class="text-left">
+                Architecture
+              </th>
+              <th class="text-left">
+                Type
+              </th>
+              <th class="text-left">
+              </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+              v-for="asset in assets"
+              :key="getAssetUrl(asset, version)"
+            >
+              <td>
+                <v-icon
+                  :color="PLATFORM_ICONS[asset.platform].color"
+                  class="mr-2"
+                >
+                  mdi-{{ PLATFORM_ICONS[asset.platform].icon }}
+                </v-icon>
+                {{ asset.platform }}
+              </td>
+              <td>
+                <code
+                  :style="{
+                    background: asset.architecture === 'amd64' ? 'green' : 'lightblue',
+                    color: asset.architecture === 'amd64' ? 'white' : 'black',
+                  }"
+                >{{ asset.architecture }}</code>
+              </td>
+              <td>
+                <v-icon
+                  :color="EXTENSION_ICONS[asset.extension].color"
+                  class="mr-2"
+                >
+                  mdi-{{ EXTENSION_ICONS[asset.extension].icon }}
+                </v-icon>
+                {{ asset.extension }}
+              </td>
+              <td>
+                <v-btn
+                  style="text-decoration: none !important;"
+                  color="primary"
+                  :href="getAssetUrl(asset, version)"
+                >
+                  Download
+                </v-btn>
+              </td>
+            </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss">
 </style>
 <script>
 import EventBus from '@/event-bus';
+
+const PLATFORM_ICONS = {
+  windows: {
+    icon: 'microsoft-windows',
+    color: 'blue',
+  },
+  freebsd: {
+    icon: 'freebsd',
+    color: 'red',
+  },
+  darwin: {
+    icon: 'apple',
+    color: 'grey',
+  },
+  linux: {
+    icon: 'linux',
+    color: 'black',
+  },
+};
+
+const EXTENSION_ICONS = {
+  deb: {
+    icon: 'debian',
+    color: 'blue',
+  },
+  rpm: {
+    icon: 'centos',
+    color: 'red',
+  },
+  zip: {
+    icon: 'folder-zip',
+    color: 'orange',
+  },
+  'tar.gz': {
+    icon: 'archive',
+    color: 'gray',
+  },
+};
 
 export default {
   components: {},
@@ -55,11 +164,62 @@ export default {
 
   data() {
     return {
+      PLATFORM_ICONS,
+      EXTENSION_ICONS,
       deleteProjectDialog: null,
+      versions: [{
+        semver: '2.9.63',
+        id: '07238e2b-6cc1-422d-b1f9-0deb45cf4d93',
+      }],
+      assets: [{
+        platform: 'linux',
+        architecture: 'amd64',
+        extension: 'tar.gz',
+      }, {
+        platform: 'linux',
+        architecture: 'arm64',
+        extension: 'tar.gz',
+      }, {
+        platform: 'linux',
+        architecture: 'amd64',
+        extension: 'deb',
+      }, {
+        platform: 'linux',
+        architecture: 'arm64',
+        extension: 'deb',
+      }, {
+        platform: 'linux',
+        architecture: 'amd64',
+        extension: 'rpm',
+      }, {
+        platform: 'linux',
+        architecture: 'arm64',
+        extension: 'rpm',
+      }, {
+        platform: 'freebsd',
+        architecture: 'amd64',
+        extension: 'tar.gz',
+      }, {
+        platform: 'freebsd',
+        architecture: 'arm64',
+        extension: 'tar.gz',
+      }, {
+        platform: 'darwin',
+        architecture: 'amd64',
+        extension: 'tar.gz',
+      }, {
+        platform: 'windows',
+        architecture: 'amd64',
+        extension: 'zip',
+      }],
     };
   },
 
   methods: {
+    getAssetUrl(asset, version) {
+      return `https://www.semui.co/uploads/v${version.semver}-premium/${version.id}/semaphore_${version.semver}-premium_${asset.platform}_${asset.architecture}.${asset.extension}`;
+    },
+
     showDrawer() {
       EventBus.$emit('i-show-drawer');
     },
