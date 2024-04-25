86dc748bf0243d2e32391ceb121ac7379ed61f62
fiftin <denguk@gmail.com>
2024-04-26 01:27:55 +0500

    fix: docker instruction

--- web/src/views/project/Install.vue
+++ web/src/views/project/Install.vue
@@ -26,7 +26,7 @@ docker run -p 3000:3000 --name semaphore \
     -e SEMAPHORE_ADMIN_PASSWORD=changeme \
     -e SEMAPHORE_ADMIN_NAME=Admin \
     -e SEMAPHORE_ADMIN_EMAIL=admin@localhost \
-    -d semaphoreui/semaphore:{{dockerGuideVersion.semver}}-premium
+    -d semaphoreui/semaphore:v{{dockerGuideVersion.semver}}-premium
           </pre>
         </v-card-text>
       </v-card>
